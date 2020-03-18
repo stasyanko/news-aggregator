f10643abcad8e86f79b1d7d00010100ffbfbe7c0
stas <kapilka06@gmail.com>
2020-03-18 11:41:25 +0300

    GetLatestNewsCommand

---
+++ src/Command/GetLatestNewsCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GetLatestNewsCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'get:latest-news';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Gets latest news from all available data sources and stores it to database.')
+            ->setHelp('Gets latest news from all available data sources and stores it to database');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        //TODO: complete
+    }
+}
