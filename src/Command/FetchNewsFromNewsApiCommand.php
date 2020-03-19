376e02227d83ca96d75dcb7caabd6edd630b0c50
stas <kapilka06@gmail.com>
2020-03-19 19:49:17 +0300

    FetchNewsCommand refactored, added database persistance

--- src/Command/FetchNewsFromNewsApiCommand.php
+++ src/Command/FetchNewsFromNewsApiCommand.php
@@ -5,32 +5,38 @@ namespace App\Command;
 
 
 use App\Entity\Article;
+use App\Exceptions\NewsFetchFailedException;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use DateTime;
+use Throwable;
 
 class FetchNewsFromNewsApiCommand implements FetchNewsCommandInterface
 {
     private HttpClientInterface $httpClient;
-    private const BASE_URL = '';
+    private LoggerInterface $logger;
 
-    public function __construct(HttpClientInterface $httpClient)
+    public function __construct(
+        HttpClientInterface $httpClient,
+        LoggerInterface $logger
+    )
     {
         $this->httpClient = $httpClient;
+        $this->logger = $logger;
     }
+
     /**
      * @return Article[]
+     * @throws NewsFetchFailedException
      */
     public function execute(): array
     {
-        //TODO: move request to API to a separate method
-        $newsFromApiJson = $this->httpClient
-            ->request(
-                'GET',
-                'http://newsapi.org/v2/top-headlines?country=us&apiKey=' . $_ENV['NEWS_API_KEY'],
-                ['http_version' => '2.0']
-            )
-            ->getContent();
-        $newsFromApiArr = json_decode($newsFromApiJson);
+        try {
+            $newsFromApiArr = $this->makeRequestToApi();
+        } catch (Throwable $throwable) {
+            $this->logger->error($throwable->getMessage());
+            throw new NewsFetchFailedException();
+        }
 
         $resArray = [];
 
@@ -38,15 +44,35 @@ class FetchNewsFromNewsApiCommand implements FetchNewsCommandInterface
             $article = new Article();
             $article->setTitle($newsItem->title);
             $article->setDescription($newsItem->description);
-            $article->setDescription($newsItem->description);
+            $article->setSource((string)$newsItem->source->id);
             $article->setUrl($newsItem->url);
             $article->setImageUrl($newsItem->urlToImage);
             $article->setPublishedAt(new DateTime());
-            $article->setContent((string) $newsItem->content);
+            $article->setContent((string)$newsItem->content);
 
             $resArray[] = $article;
         }
 
         return $resArray;
+    }
+
+    /**
+     * @return array
+     * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface
+     * @throws \Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface
+     */
+    private function makeRequestToApi(): object
+    {
+        $newsFromApiJson = $this->httpClient
+            ->request(
+                'GET',
+                'http://newsapi.org/v2/top-headlines?country=us&apiKey=' . $_ENV['NEWS_API_KEY'],
+                ['http_version' => '2.0']
+            )
+            ->getContent();
+
+        return json_decode($newsFromApiJson);
     }
 }
--- src/Console/GetLatestNewsConsoleCommand.php
+++ src/Console/GetLatestNewsConsoleCommand.php
@@ -5,15 +5,29 @@ namespace App\Console;
 
 use App\Command\FetchNewsFromNewsApiCommand;
 use App\Command\Invoker\NewsCommandInvoker;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpClient\CurlHttpClient;
+use Symfony\Component\HttpKernel\Log\Logger;
 
 class GetLatestNewsConsoleCommand extends Command
 {
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'get:latest-news';
+    private EntityManagerInterface $em;
+
+    /**
+     * OneLevel Constructor
+     *
+     * @param EntityManagerInterface $em
+     */
+    public function __construct(EntityManagerInterface $em)
+    {
+        parent::__construct();
+        $this->em = $em;
+    }
 
     protected function configure()
     {
@@ -22,18 +36,28 @@ class GetLatestNewsConsoleCommand extends Command
             ->setHelp('Gets latest news from all available data sources and stores it to database');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): void
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $newsDataSources = [
-            new FetchNewsFromNewsApiCommand(new CurlHttpClient()),
+            new FetchNewsFromNewsApiCommand(
+                new CurlHttpClient(),
+                new Logger()
+            ),
         ];
 
         $commandInvoker = new NewsCommandInvoker();
 
         foreach ($newsDataSources as $newsDataSource) {
             $articles = $commandInvoker->execute($newsDataSource);
-            //TODO: store to db
-            dd($articles);
+            //TODO: check if an article exists in db
+            foreach($articles as $article) {
+                $this->em->persist($article);
+            }
+
+            $this->em->flush();
+            $this->em->clear();
         }
+
+        return 0;
     }
 }
